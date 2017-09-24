171951075f5926a1487623e773cbeb4a0fb7a98f
lpz <lpz>
2017-09-24 20:24:25 +0800

    哈哈哈

---
+++ Demo44.java
@@ -0,0 +1,14 @@
+
+public class Demo44 {
+  public static void main(String[] args){
+	  int ary[]=new int[2];
+	  ary[0]=1;
+	  ary[1]=2;	
+	  System.out.println("ary[0]="+ary[0]+";ary[1]="+ary[1]);
+	  double stuScore[]=new double[5];
+	  for(i=0;i<5;i++){
+		  stuScore[i]=(i+1)*10;
+	  }
+	  System.out.println("stuScore["+i+"]");
+  }
+}
